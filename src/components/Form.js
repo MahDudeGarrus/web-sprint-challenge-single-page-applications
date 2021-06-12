21ccf20054bac7cd7271f80a4a105463da9cd1ca
Stephanie Ann Solis <stephaniesolis@lambdastudents.com>
2021-06-12 15:28:25 -0600

    added on change handler and passed it to inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -34,31 +34,31 @@ export default function Form(props) {
             
             <div className='sauceSelect'>
                 <h3>Choice of Sauce</h3>
-                    <label> Tomato Sauce <input type='radio' name='sauce' onChange={changes}/> </label>
-                    <label> Garlic Alfredo <input type='radio' name='sauce' onChange={changes}/> </label>
-                    <label> Butter Parmesan <input type='radio' name='sauce' onChange={changes}/> </label>
-                    <label> Triple Cheese Cream <input type='radio' name='sauce' onChange={changes}/> </label>
-                    <label> Sundried Tomato Olive Oil <input type='radio' name='sauce' onChange={changes}/> </label>
+                    <label> Tomato Sauce <input type='radio' name='sauce' value='tomato'onChange={changes}/> </label>
+                    <label> Garlic Alfredo <input type='radio' name='sauce' value='garlic' onChange={changes}/> </label>
+                    <label> Butter Parmesan <input type='radio' name='sauce' value='butter' onChange={changes}/> </label>
+                    <label> Triple Cheese Cream <input type='radio' name='sauce' value='triple' onChange={changes}/> </label>
+                    <label> Sundried Tomato Olive Oil <input type='radio' name='sauce' value='sundried' onChange={changes}/> </label>
             </div>
 
             <div className='toppingsSelect'>
                 <h3> Add Toppings</h3>
                     <p>Choose up to 10</p>
 
-                    <label> pepperoni <input type='checkbox' name="toppings1" onChange={changes}/> </label>
-                    <label> italian sausage <input type='checkbox' name="toppings2" onChange={changes}/> </label>
-                    <label> bacon <input type='checkbox' name="toppings3" onChange={changes}/> </label>
-                    <label> mushrooms <input type='checkbox' name="toppings4" onChange={changes}/> </label>
-                    <label> black olives <input type='checkbox' name="toppings5" onChange={changes}/> </label>
-                    <label> bell peppers <input type='checkbox' name="toppings6" onChange={changes}/> </label>
-                    <label> red onion <input type='checkbox' name="toppings7" onChange={changes}/> </label>
-                    <label> feta cheese <input type='checkbox' name="toppings8" onChange={changes}/> </label>
-                    <label> extra mozzerella <input type='checkbox' name="toppings9" onChange={changes}/> </label>
-                    <label> spinach <input type='checkbox' name="toppings10" onChange={changes}/> </label>
-                    <label> grilled chicken <input type='checkbox' name="toppings11" onChange={changes}/> </label>
-                    <label> ham <input type='checkbox' name="toppings12" onChange={changes}/> </label>
-                    <label> pineapple <input type='checkbox' name="toppings13" onChange={changes}/> </label>
-                    <label> sardines <input type='checkbox' name="toppings14" onChange={changes}/> </label>
+                    <label> pepperoni <input type='checkbox' name="toppings1" value='1' onChange={changes}/> </label>
+                    <label> italian sausage <input type='checkbox' name="toppings2" value='2' onChange={changes}/> </label>
+                    <label> bacon <input type='checkbox' name="toppings3" value='3' onChange={changes}/> </label>
+                    <label> mushrooms <input type='checkbox' name="toppings4" value='4' onChange={changes}/> </label>
+                    <label> black olives <input type='checkbox' name="toppings5" value='5' onChange={changes}/> </label>
+                    <label> bell peppers <input type='checkbox' name="toppings6" value='6' onChange={changes}/> </label>
+                    <label> red onion <input type='checkbox' name="toppings7" value='7' onChange={changes}/> </label>
+                    <label> feta cheese <input type='checkbox' name="toppings8" value='8' onChange={changes}/> </label>
+                    <label> extra mozzerella <input type='checkbox' name="toppings9" value= '9' onChange={changes}/> </label>
+                    <label> spinach <input type='checkbox' name="toppings10" value='10' onChange={changes}/> </label>
+                    <label> grilled chicken <input type='checkbox' name="toppings11" value='11' onChange={changes}/> </label>
+                    <label> ham <input type='checkbox' name="toppings12" value='12' onChange={changes}/> </label>
+                    <label> pineapple <input type='checkbox' name="toppings13" value='13' onChange={changes}/> </label>
+                    <label> sardines <input type='checkbox' name="toppings14" value='14' onChange={changes}/> </label>
             </div>
 
             <div>
